b75e0c507a97bab3cffe68f9dec4550ca8c21e43
Kyriakos Toumbas <kyriakos@pixelactions.com>
2015-12-18 18:30:55 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='cec_backend_python',
+    version='0.0.1',
+    packages=['cec_backend_python'],
+    description='CEC Backend Python API client',
+    author='Kyriakos Toumbas',
+    author_email='kyriakos@pixelactions.com',
+    url='https://github.com/PixelActions/cec_backend_python',
+    install_requires=['requests >= 2.2.1'],
+    license='http://opensource.org/licenses/MIT',
+    #test_suite='tests',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+    keywords='cec_backend api'
+)
